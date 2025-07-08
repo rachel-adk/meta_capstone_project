962700b6195aca19bfa2b5f2c3e45ab04bd40748
Rachel Sena Adorkor <racheladksena@gmail.com>
2025-07-08 07:04:11 -0700

    Worked on sidebar for all the pages

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,19 +1,31 @@
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { BrowserRouter as Router, Route, Routes, useNavigate } from "react-router-dom";
+import { useState } from "react";
 import SignupForm from "./components/SignupForm";
 import LoginForm from "./components/LoginForm";
 import HomePage from "./components/HomePage";
 import MedicalHistory from "./components/MedicalHistory";
 import SymptomsPage from "./components/SymptomsPage";
 import AllergiesPage from "./components/AllergiesPage";
+import Sidebar from "./components/Sidebar";
+
 
 const App = () => {
+
+  const [isOpen, setIsOpen] = useState(false);
   return (
     <div className="flex flex-col min-h-screen w-full">
       <Router>
         <header className="bg-teal-700 flex justify-center items-center p-8">
           <h1 className="text-white text-6xl font-bold">🩺 HealthConnect 🩺</h1>
         </header>
-        <main className="flex-grow">
+
+
+        <div className="flex">
+          <Sidebar isOpen={isOpen} setIsOpen={setIsOpen} />
+        <main
+        className={`flex-grow transition-all duration-300 ${
+          isOpen ? "ml-60" : "ml-0"
+        }`}>
           <Routes>
             <Route path="/" element={<HomePage />} />
             <Route path="/signup" element={<SignupForm />} />
@@ -23,12 +35,17 @@ const App = () => {
             <Route path="/allergies" element={<AllergiesPage />} />
           </Routes>
           <div className="bg-teal-700 text-white py-2 px-3 text-left">
-            <p>2025 HealthConnect</p>
+            <p>©2025 HealthConnect</p>
           </div>
         </main>
+        </div>
       </Router>
-    </div>
-  );
+      </div>
+    );
+
+
+
+
 };
 
 export default App;
