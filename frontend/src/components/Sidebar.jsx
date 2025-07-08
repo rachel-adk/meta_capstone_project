1f076ed99238427542b47012ac5a46da03767093
Rachel Sena Adorkor <racheladksena@gmail.com>
2025-07-08 09:35:30 -0700

    Fixed formatting for code

--- frontend/src/components/Sidebar.jsx
+++ frontend/src/components/Sidebar.jsx
@@ -1,69 +1,66 @@
 import { useNavigate } from "react-router-dom";
 import { useState } from "react";
 
-
 const Sidebar = () => {
-    const navigate = useNavigate();
-    const [isOpen, setIsOpen ] = useState(false)
+  const navigate = useNavigate();
+  const [isOpen, setIsOpen] = useState(false);
 
-    return (
-        <>
-        <button
+  return (
+    <>
+      <button
         onClick={() => setIsOpen(!isOpen)}
-        className="fixed top-4 left-4 z-50 bg-teal-300 text-white p-2 rounded-md hover:bg-teal-800 w-10 flex-row"> 🟰 </button>
+        className="fixed top-4 left-4 z-50 bg-teal-300 text-white p-2 rounded-md hover:bg-teal-800 w-10 flex-row"
+      >
+        {" "}
+        🟰{" "}
+      </button>
 
-        <div
-            className={`h-screen bg-white border-r border-gray-200 shadow transition-all duration-300 ease-in-out ${isOpen ? "w-70" : "w-0"
-
-            } overflow-hidden`}>
-
-
-
-            <div className="p-6 flex flex-col gap-4">
-             <button
-             onClick={() => {
-                 navigate("/");
-                 setIsOpen(false);
-             }}
-             className="text-left text-teal-700 hover:bg-teal-100 px-3 py-2 rounded">
-                 Home</button>
-             <button
-             onClick={() => {
-                 navigate("/med_history");
-                 setIsOpen(false);
-             }}
-             className="text-left text-teal-700 hover:bg-teal-100 px-3 py-2 rounded">
-                 Medical History</button>
-             <button
-             onClick={() => {
-                 navigate("/symptoms");
-                 setIsOpen(false);
-             }}
-             className="text-left text-teal-700 hover:bg-teal-100 px-3 py-2 rounded">
-                Symptoms</button>
-
-             <button
+      <div
+        className={`h-screen bg-white border-r border-gray-200 shadow transition-all duration-300 ease-in-out ${
+          isOpen ? "w-70" : "w-0"
+        } overflow-hidden`}
+      >
+        <div className="p-6 flex flex-col gap-4">
+          <button
             onClick={() => {
-                navigate("/allergies");
-                setIsOpen(false);
+              navigate("/");
+              setIsOpen(false);
             }}
-            className="text-left text-teal-700 hover:bg-teal-100 px-3 py-2 rounded">
-                Allergies</button>
+            className="text-left text-teal-700 hover:bg-teal-100 px-3 py-2 rounded"
+          >
+            Home
+          </button>
+          <button
+            onClick={() => {
+              navigate("/med_history");
+              setIsOpen(false);
+            }}
+            className="text-left text-teal-700 hover:bg-teal-100 px-3 py-2 rounded"
+          >
+            Medical History
+          </button>
+          <button
+            onClick={() => {
+              navigate("/symptoms");
+              setIsOpen(false);
+            }}
+            className="text-left text-teal-700 hover:bg-teal-100 px-3 py-2 rounded"
+          >
+            Symptoms
+          </button>
 
+          <button
+            onClick={() => {
+              navigate("/allergies");
+              setIsOpen(false);
+            }}
+            className="text-left text-teal-700 hover:bg-teal-100 px-3 py-2 rounded"
+          >
+            Allergies
+          </button>
         </div>
-        </div>
-
-        {/* <div className={`flex-1 transition-all duration -300 ${
-            isOpen ? "ml-100" : "ml-0"
-        }`}>
-
-        </div> */}
-
-        </>
-
-
-
-
-        )
-}
+      </div>
+    </>
+  );
+};
 export default Sidebar;
