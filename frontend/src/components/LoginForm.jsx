e50f07887fe80941b2be85376f5ee35c903fda72
Rachel Sena Adorkor <racheladksena@gmail.com>
2025-07-08 21:01:18 -0700

    worked on route for logout feature

--- frontend/src/components/LoginForm.jsx
+++ frontend/src/components/LoginForm.jsx
@@ -1,6 +1,7 @@
 import { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import { Link } from "react-router-dom";
+import { useAuthContext } from "../context/AuthContext";
 
 
 const LoginForm = () => {
