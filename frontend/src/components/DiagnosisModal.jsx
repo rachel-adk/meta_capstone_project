2f0ace0732651fb25c16bc90293a2409af2b01fa
Rachel Sena Adorkor <racheladksena@gmail.com>
2025-07-16 19:08:31 -0700

    Worked on fixing issues I had with displaying modal for diagnosis feature

--- frontend/src/components/DiagnosisModal.jsx
+++ frontend/src/components/DiagnosisModal.jsx
@@ -1,31 +1,38 @@
 import React from "react";
 
-const DiagnosisModal = ({ showModal, setModal, diagnosisResult }) => {
+const DiagnosisModal = ({ showModal, onClose, diagnosisResult, symptoms }) => {
   if (!showModal) {
     return null;
   }
 
   return (
     <div className="fixed inset-0 z-50 flex items-center justify-center bg-opacity-20">
-      <div className="bg-teal-600 rounded-lg w-full max-w-md relative">
+      <div className="bg-teal-200 rounded-lg w-full max-w-md relative">
         <button
-          onClick={() => setModal(false)}
+          onClick={onClose}
           className="absolute top-2 right-2 text-gray-500"
         >
-          {" "}
-          ❌{" "}
+
+          x
         </button>
-        <h2 className="text-xl font-semibold text-teal-700">Diagnosis</h2>
+        <h2 className="text-xl font-semibold text-teal-900 p-4">Diagnosis</h2>
 
         {diagnosisResult ? (
           <>
-            <p className="mb-2">Disease: {diagnosisResult.condition}</p>
-            <p className="mb-2">Precautions: </p>
-            <ul className="list-disc ml-6">
+            <h3 className="teal-900 text-center">Summary</h3>
+            <p className="mb-2 px-2">Symptoms: </p>
+             <ul className="list-disc ml-6">
+              {symptoms.map((symptom, index) => (
+                <li key={index}>{symptom}</li>
+              ))}
+            </ul>
+            <p className="mb-2 p-4">Disease: {diagnosisResult.condition}</p>
+            <p className="mb-2 p-4">Precautions: </p>
+            {/* <ul className="list-disc ml-6">
               {diagnosisResult.precaution.map((precaution, index) => (
                 <li key={index}>{precaution}</li>
               ))}
-            </ul>
+            </ul> */}
           </>
         ) : (
           <p>Loading...</p>
