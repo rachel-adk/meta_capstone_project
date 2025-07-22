4673e676fad1bf42a3181d37a664d264e3ea7ac4
Rachel Sena Adorkor <racheladksena@gmail.com>
2025-07-22 11:58:47 -0700

    Worked on maps feature in frontend

---
+++ frontend/src/components/HospitalsPage.jsx
@@ -0,0 +1,67 @@
+import { useState } from "react";
+import { getHospitals } from "../utils/data";
+import HospitalCard from "./HospitalCard";
+import MapView from "./MapView";
+
+
+export default function HospitalsPage() {
+  const [hospitals, setHospitals] = useState([]);
+  const [error, setError] = useState("");
+  const [asked, setAsked] = useState(false);
+  const [loc, setLoc] = useState(null);
+
+  // Getting the user's location and displaying the nearby hospitals
+  async function handleUseLocation() {
+    setAsked(true);
+    try {
+      const { latitude, longitude } = await new Promise((resolve, reject) => {
+        navigator.geolocation.getCurrentPosition(
+          (pos) => resolve(pos.coords),
+          (err) => reject(err.message),
+          { enableHighAccuracy: true, timeout: 5000 }
+        );
+      });
+      setLoc({ latitude, longitude });
+      const hospitalList = await getHospitals(latitude, longitude);
+      setHospitals(hospitalList);
+    } catch (err) {
+      setError(err);
+    }
+  }
+
+  if (!asked) {
+    return (
+      <div className="flex item-center justify-center h-screen">
+        <div className="text-center p-6 bg-white rounded shadow">
+          <h2 className="text-2xl font-bold">Find Nearby Hospitals</h2>
+          <p className="text-lg">We would like to use your locatiom</p>
+          <button
+            onClick={handleUseLocation}
+            className="bg-teal-500 px-3 py -2 text-white"
+          >
+            Use my location
+          </button>
+        </div>
+      </div>
+    );
+  }
+  if (error) {
+    return (
+      <div className="p-4 text-red-700">
+        <p>Error: {error}</p>
+      </div>
+    );
+  }
+  return (
+    <div className="flex h-screen">
+      <aside className="w-1/3 overflow-y-auto p-4 bg-gray-50">
+        {hospitals.map((h) => (
+          <HospitalCard key={h.id} hospital={h} />
+        ))}
+      </aside>
+      <main className="flex-1">
+        <MapView hospitals={hospitals} center={loc} />
+      </main>
+    </div>
+  );
+}
--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -3,6 +3,7 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 import { UserProvider } from './contexts/UserContext.jsx'
+import L from 'leaflet'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
@@ -11,3 +12,9 @@ createRoot(document.getElementById('root')).render(
     </UserProvider>
   </StrictMode>,
 )
+delete L.Icon.Default.prototype._getIconUrl
+L.IconDefault.mergeOptions({
+  iconRetinaUrl: require('leaflet/dist/images/marker-icon-2x.png'),
+  iconUrl: require('leaflet/dist/images/marker-icon.png'),
+  shadowUrl: require('leaflet/dist/images/marker-shadow.png'),
+})
